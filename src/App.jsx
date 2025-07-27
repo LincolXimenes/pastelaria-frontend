9a50f57a657386f69f155563ad5828dc99fcae37
LincolXimenes <lincolnximenes19@gmail.com>
2025-07-27 15:56:46 -0300

    feat: estrutura inicial da página de Home com cards e modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,7 @@
-import React, { useState } from 'react';
-import Modal from './components/Modal';
+import Home from './pages/Home';
 
 function App() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  return (
-    <div className="p-6">
-      <button
-        onClick={() => setIsModalOpen(true)}
-        className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
-      >
-        Abrir Modal
-      </button>
-
-      <Modal
-        isOpen={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
-        title="Título do Modal"
-      >
-        <p>Conteúdo interno do modal.</p>
-      </Modal>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
